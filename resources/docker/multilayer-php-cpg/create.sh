e9a7456c44d523e669169e70013a725915c44ea7
Jannik Hartung <jannik.hartung@tu-bs.de>
2025-05-26 19:20:09 +0200

    Switch to HTTPS git clone instead of SSH
    So you don't have to have your SSH keys configured for github.com.

--- resources/docker/multilayer-php-cpg/create.sh
+++ resources/docker/multilayer-php-cpg/create.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 branch=${1:-"master"}
- DOCKER_BUILDKIT=1 docker build --ssh default --no-cache --build-arg BRANCH=${branch} -t multilayer-cpg-php .
+ DOCKER_BUILDKIT=1 docker build --no-cache --build-arg BRANCH=${branch} -t multilayer-cpg-php .
